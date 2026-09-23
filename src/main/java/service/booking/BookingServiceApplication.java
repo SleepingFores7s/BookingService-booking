e82c8843159c9a300dc0e65f6a03788e63057fd8
Daniel Evensson <daniel.evensson@hotmail.com>
2026-09-23 08:52:37 +0200

    added test comment

--- src/main/java/service/booking/BookingServiceApplication.java
+++ src/main/java/service/booking/BookingServiceApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BookingServiceApplication {
 
+    //test commit comment
+
     public static void main(String[] args) {
         SpringApplication.run(BookingServiceApplication.class, args);
     }
